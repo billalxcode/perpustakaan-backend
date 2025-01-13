79d432f10c07963428322a3e9d7b937f233a9cac
billalxcode <billal.xcode@gmail.com>
2025-01-13 16:37:44 +0700

    Implement FilamentUser interface in User model and add access control for panels based on user roles

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,12 +3,15 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable, HasRoles;
@@ -46,5 +49,20 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        $panelID = $panel->getId();
+
+        if ($panelID === "admin") {
+            return $this->hasRole('admin');
+        } else if ($panelID == "officer") {
+            return $this->hasRole('officer');
+        } else if ($panelID == "member") {
+            return $this->hasRole('member');
+        } else {
+            return false;
+        }
     }
 }
