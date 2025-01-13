670784cdddf6bb1b761661cfabe578869eae8df3
billalxcode <billal.xcode@gmail.com>
2025-01-13 16:33:17 +0700

    Initialize Laravel project structure with essential files and configurations

---
+++ bootstrap/providers.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    App\Providers\AppServiceProvider::class,
+    App\Providers\Filament\AdminPanelProvider::class,
+    App\Providers\Filament\MemberPanelProvider::class,
+    App\Providers\Filament\OfficerPanelProvider::class,
+];
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Role::findOrCreate('admin');
+        Role::findOrCreate('officer');
+        Role::findOrCreate('member');
+
+        $admin = User::create([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('password'),
+        ]);
+        $admin->assignRole('admin');
+
+        $officer = User::create([
+            'name' => 'Officer',
+            'email' => 'officer@admin.com',
+            'password' => bcrypt('password'),
+        ]);
+        $officer->assignRole('officer');
+
+        $member = User::create([
+            'name' => 'Member',
+            'email' => 'member@admin.com',
+            'password' => bcrypt('password'),
+        ]);
+        $member->assignRole('member');
+    }
+}
