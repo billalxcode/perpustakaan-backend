c65c280c1922a800dea1f5e73f9aef4525b0d2ed
billalxcode <billal.xcode@gmail.com>
2025-01-14 11:05:57 +0700

    refactor: remove unused import in CreateBookCategory page

--- app/Filament/Resources/BookCategoryResource/Pages/CreateBookCategory.php
+++ app/Filament/Resources/BookCategoryResource/Pages/CreateBookCategory.php
@@ -3,7 +3,6 @@
 namespace App\Filament\Resources\BookCategoryResource\Pages;
 
 use App\Filament\Resources\BookCategoryResource;
-use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateBookCategory extends CreateRecord
