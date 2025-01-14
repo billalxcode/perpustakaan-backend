c6af70da9b43d8d3c255603d9fc4b1569240fd78
billalxcode <billal.xcode@gmail.com>
2025-01-14 11:04:45 +0700

    feat: add BookCategoryResource for managing book categories in Filament

---
+++ app/Filament/Resources/BookCategoryResource.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\BookCategoryResource\Pages;
+use App\Filament\Resources\BookCategoryResource\RelationManagers;
+use App\Models\BookCategory;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class BookCategoryResource extends Resource
+{
+    protected static ?string $model = BookCategory::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListBookCategories::route('/'),
+            'create' => Pages\CreateBookCategory::route('/create'),
+            'edit' => Pages\EditBookCategory::route('/{record}/edit'),
+        ];
+    }
+}
