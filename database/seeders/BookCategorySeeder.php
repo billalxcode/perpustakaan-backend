df38979c5e01d08ee8c1e798ab061028545ec3b6
billalxcode <billal.xcode@gmail.com>
2025-01-14 11:06:01 +0700

    refactor: remove unused import in BookCategorySeeder

--- database/seeders/BookCategorySeeder.php
+++ database/seeders/BookCategorySeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\BookCategory;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class BookCategorySeeder extends Seeder
