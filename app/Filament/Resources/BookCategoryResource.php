7a13459c290ef0f0e2eacd327832875236b874d6
billalxcode <billal.xcode@gmail.com>
2025-01-14 11:05:53 +0700

    refactor: remove unused imports in BookCategoryResource

--- app/Filament/Resources/BookCategoryResource.php
+++ app/Filament/Resources/BookCategoryResource.php
@@ -3,15 +3,11 @@
 namespace App\Filament\Resources;
 
 use App\Filament\Resources\BookCategoryResource\Pages;
-use App\Filament\Resources\BookCategoryResource\RelationManagers;
 use App\Models\BookCategory;
-use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class BookCategoryResource extends Resource
 {
