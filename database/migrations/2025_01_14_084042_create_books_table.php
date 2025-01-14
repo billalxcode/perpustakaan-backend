1be4cb26f42a2dfe76bed0645ca2427ed03c3f66
billalxcode <billal.xcode@gmail.com>
2025-01-14 16:52:17 +0700

    refactor: clean up unused imports and formatting in resource files

--- database/migrations/2025_01_14_084042_create_books_table.php
+++ database/migrations/2025_01_14_084042_create_books_table.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Models\BookCategory;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -14,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('books', function (Blueprint $table) {
             $table->id();
-            
+
             $table->string('name');
             $table->string('author');
             $table->string('isbn');
