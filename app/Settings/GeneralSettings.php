481bfe7a8a5b9b397348170ae89e022f6000be10
billalxcode <billal.xcode@gmail.com>
2025-01-23 23:32:27 +0700

    feat: add locale property to GeneralSettings class

--- app/Settings/GeneralSettings.php
+++ app/Settings/GeneralSettings.php
@@ -7,7 +7,8 @@ use Spatie\LaravelSettings\Settings;
 class GeneralSettings extends Settings
 {
     public string $primary_theme_color;
-
+    public string $locale;
+    
     public static function group(): string
     {
         return 'general';
