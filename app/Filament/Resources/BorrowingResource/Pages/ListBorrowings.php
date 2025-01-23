1fd2fc2acd9261c3d1fae9fd98675f4c9c7ebf81
billalxcode <billal.xcode@gmail.com>
2025-01-23 21:32:35 +0700

    feat: add tabbed interface for listing borrowings by status

--- app/Filament/Resources/BorrowingResource/Pages/ListBorrowings.php
+++ app/Filament/Resources/BorrowingResource/Pages/ListBorrowings.php
@@ -4,7 +4,9 @@ namespace App\Filament\Resources\BorrowingResource\Pages;
 
 use App\Filament\Resources\BorrowingResource;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListBorrowings extends ListRecords
 {
@@ -14,6 +16,17 @@ class ListBorrowings extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'all' => Tab::make('All Borrowings'),
+            'borrowed' => Tab::make('Borrowed')
+                ->modifyQueryUsing(fn(Builder $query) => $query->where('status', '=', 'borrowed')),
+            'returned' => Tab::make('Returned')
+                ->modifyQueryUsing(fn(Builder $query) => $query->where('status', '=', 'returned')),
         ];
     }
 }
