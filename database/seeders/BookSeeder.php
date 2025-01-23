3939428a84a2cce3d825edf8991131313445e320
billalxcode <billal.xcode@gmail.com>
2025-01-23 18:42:41 +0700

    feat: update BookSeeder to create a specific book record with detailed attributes

--- database/seeders/BookSeeder.php
+++ database/seeders/BookSeeder.php
@@ -12,6 +12,16 @@ class BookSeeder extends Seeder
      */
     public function run(): void
     {
-        Book::factory()->count(30)->create();
+        Book::factory(1)->create([
+            'name' => "Al-Qur'An Cordoba Al-Hufaz Ka'Bah Hafalan Mudah A5",
+            'author' => "Gramedia Widiasarana Indonesia",
+            'isbn' => "9786026765840",
+            "cover_image" => "https://cdn.gramedia.com/uploads/products/aaonh870u5.jpeg",
+            "description" => "Al-Qur'an Cordoba Al-Hufaz Ka'Bah Hafalan Mudah A5 adalah Al-Qur'an yang dilengkapi dengan tajwid berwarna dan terjemahan. Al-Qur'an ini cocok untuk dijadikan sebagai hadiah atau kenang-kenangan. Al-Qur'an ini juga cocok untuk dijadikan sebagai hadiah atau kenang-kenangan.",
+            "publisher" => "Gramedia Widiasarana Indonesia",
+            "published_at" => "2020-03-06",
+            "pages" => 604,
+            "category_id" => 1,
+        ]);
     }
 }
