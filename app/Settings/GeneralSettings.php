fb8c0dafbb36f4be24a55edd0f90ff8df553e573
billalxcode <billal.xcode@gmail.com>
2025-01-23 22:56:32 +0700

    feat: create GeneralSettings class for managing primary theme color configuration

---
+++ app/Settings/GeneralSettings.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Settings;
+
+use Spatie\LaravelSettings\Settings;
+
+class GeneralSettings extends Settings
+{
+    public string $primary_theme_color;
+    
+    public static function group(): string
+    {
+        return 'general';
+    }
+}
