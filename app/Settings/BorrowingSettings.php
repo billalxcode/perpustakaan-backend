06f328b33ef5ca6e18c37231514c97e1aeead34a
billalxcode <billal.xcode@gmail.com>
2025-01-23 21:32:43 +0700

    feat: add BorrowingSettings class for managing borrowing configuration

---
+++ app/Settings/BorrowingSettings.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Settings;
+
+use Spatie\LaravelSettings\Settings;
+
+class BorrowingSettings extends Settings
+{
+    public string $borrowing_period;
+    public string $borrowing_limit;
+    public string $borrowing_fine;
+
+    public static function group(): string
+    {
+        return 'borrowing';
+    }
+}
