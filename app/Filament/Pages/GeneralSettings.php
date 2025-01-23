5a18bf77d05737023dca85b70aae402a09ee2573
billalxcode <billal.xcode@gmail.com>
2025-01-23 22:56:21 +0700

    feat: add GeneralSettings page for configuring primary theme color

---
+++ app/Filament/Pages/GeneralSettings.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use App\Settings\GeneralSettings as Settings;
+use Filament\Forms;
+use Filament\Forms\Components\ColorPicker;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Form;
+use Filament\Pages\SettingsPage;
+
+class GeneralSettings extends SettingsPage
+{
+    protected static ?string $navigationIcon = 'heroicon-o-cog-6-tooth';
+
+    protected static string $settings = Settings::class;
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Section::make("General Settings")
+                    ->schema([
+                        ColorPicker::make("primary_theme_color")
+                            ->label("Primary Theme Color")
+                            ->default("#22c55e"),
+                    ])
+            ]);
+    }
+}
