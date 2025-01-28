08e27fbdabb0587ff926c536182f3e94aeffc302
billalxcode <billal.xcode@gmail.com>
2025-01-28 07:28:37 +0700

    feat: update API routes for authentication and book management

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,30 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\BookCategoryController;
+use App\Http\Controllers\Api\BookController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::group(['as' => 'auth.', 'prefix' => 'auth', 'middleware' => 'guest'], function () {
+    Route::post('/register', [AuthController::class, 'register'])->name('register');
+    Route::post('/login', [AuthController::class,'login'])->name('login');
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', [UserController::class, 'index'])->name('user.index');
+    Route::post('/logout', [AuthController::class, 'logout'])->name('auth.logout');
+
+    Route::prefix('books')->group(function () {
+        Route::get('/', [BookController::class, 'index'])->name('books.index');
+    });
+
+    Route::prefix('categories')->group(function () {
+        Route::get('/', [BookCategoryController::class, 'index'])->name('categories.index');
+    });
+});
